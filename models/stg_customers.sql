6adbeba3b5bf5a3532be39fef27e684b18f644f6
Ronald Weening <rweening@vacanceselect.com>
2024-05-28 11:50:51 +0200

    Initial

---
+++ models/stg_customers.sql
@@ -0,0 +1,2 @@
+select id as customer_id, first_name, last_name
+from `ecg-big-data-sandbox`.dbt_models.jaffle_shop_customers
---
+++ models/stg_orders.sql
@@ -0,0 +1,2 @@
+select id as order_id, user_id as customer_id, order_date, status
+from `ecg-big-data-sandbox`.dbt_models.jaffle_shop_orders
